39d29664c7b63a7ca4009afe099062134a8c8907
Sam Creviston <samuelcreviston@gmail.com>
2024-06-06 21:39:10 -0400

    beginning index

---
+++ scripts/warhammer.js
@@ -0,0 +1,13 @@
+// Responsive footer
+const cYearElement = document.getElementById("currentyear");
+const lastModElement = document.getElementById("lastModified");
+
+if (cYearElement) {
+    const currentYear = new Date().getFullYear();
+    cYearElement.textContent = currentYear;
+}
+
+if (lastModElement) {
+    const modified = new Date(document.lastModified);
+    lastModElement.textContent = "last modified: " + modified;
+}
